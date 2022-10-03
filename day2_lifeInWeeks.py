17a94ddffbb7377b421208f075aba417a53d8b66
Naga <nagan.perni@gmail.com>
2022-10-03 16:44:49 -0500

    Life in Weeks

---
+++ day2_lifeInWeeks.py
@@ -0,0 +1,23 @@
+"""
+ was reading this article by Tim Urban - Your Life in Weeks and realised just how little time we actually have.
+https://waitbutwhy.com/2014/05/life-weeks.html
+Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+It will take your current age as the input and output a message with our time left in this format:
+You have x days, y weeks, and z months left.
+Where x, y and z are replaced with the actual calculated numbers.
+"""
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age = int(age)
+
+years_remaining = 90 - age
+
+days = years_remaining * 365
+weeks = years_remaining * 52
+months = years_remaining * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
