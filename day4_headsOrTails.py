7ec78ca627b98cfd89f5f9ccb52d9d0f72baecc3
Naga <nagan.perni@gmail.com>
2022-10-04 15:49:03 -0500

    Heads or Tails Random

---
+++ day4_headsOrTails.py
@@ -0,0 +1,16 @@
+"""
+You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+"""
+
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+random_side = random.randint(0,1)
+
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
